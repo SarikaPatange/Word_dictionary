9ed1c97285a2a9ebb7e236f12cb8c06654b1c6a0
Sarika Patange <128057504+SarikaPatange@users.noreply.github.com>
2023-08-07 04:58:49 +0000

    dictionary updated

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:search_word)
+end
+
+get("/search_meaning") do
+  @search_word = params.fetch("search_word").to_s
+  @dictionary_url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+@search_word
+  @raw_response = HTTP.get(@dictionary_url)
+  @parsed_response = JSON.parse(@raw_response)
+  @raw_response_array = @parsed_response.at(0)
+  @meanings_hash = @raw_response_array.fetch("meanings")
+  @definition_array = @meanings_hash.at(0)
+  @definition_hash = @definition_array.fetch("definitions")
+  @definition_array1 = @definition_hash.at(0)
+  @definition_hash2 = @definition_array1.fetch("definition")
+
+  erb(:search_word_results)
 end
